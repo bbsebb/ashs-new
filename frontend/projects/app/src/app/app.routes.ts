c0692a49e9c07dc23c5f20635830fffa19ee2b19
bbsebb <sebastien.burckhardt@gmail.com>
2026-04-09 18:01:57 +0200

    feat(app): implement lazy loading for all public routes

--- frontend/projects/app/src/app/app.routes.ts
+++ frontend/projects/app/src/app/app.routes.ts
@@ -1,16 +1,24 @@
 import {Routes} from '@angular/router';
-import {FeedsList} from './features/meta/components/feeds-list/feeds-list';
 import {Contact, Error404, MentionsLegales, Rgpd} from '@shared-ui';
-import {HallsList} from './features/hall/components/halls-list/halls-list';
-import {TeamsList} from "./features/team/components/teams-list/teams-list";
-import {StaffsList} from "./features/staff/components/staffs-list/staffs-list";
 
 export const routes: Routes = [
   {path: '', redirectTo: 'feeds', pathMatch: 'full'},
-  {path: 'halls', component: HallsList},
-  {path: 'teams', component: TeamsList},
-  {path: 'staffs', component: StaffsList},
-  {path: 'feeds', component: FeedsList},
+  {
+    path: 'halls',
+    loadComponent: () => import('./features/hall/components/halls-list/halls-list').then(m => m.HallsList)
+  },
+  {
+    path: 'teams',
+    loadComponent: () => import('./features/team/components/teams-list/teams-list').then(m => m.TeamsList)
+  },
+  {
+    path: 'staffs',
+    loadComponent: () => import('./features/staff/components/staffs-list/staffs-list').then(m => m.StaffsList)
+  },
+  {
+    path: 'feeds',
+    loadComponent: () => import('./features/meta/components/feeds-list/feeds-list').then(m => m.FeedsList)
+  },
   {path: 'mentions-legales', component: MentionsLegales},
   {path: 'contact', component: Contact},
   {path: 'rgpd', component: Rgpd},

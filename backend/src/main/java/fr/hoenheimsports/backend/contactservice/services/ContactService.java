783b596a0a8117f7f9a0ebe0d8a01b85f05ba56e
bbsebb <sebastien.burckhardt@gmail.com>
2026-04-30 00:07:42 +0200

    feat: externalize administrator email using configuration properties
    - Replaced hardcoded email in `ContactService` with a configurable property using `@Value`.
    - Updated `application-dev.yml`, `application-staging.yml`, and `application-prod.yml` to include environment-specific email addresses.

--- backend/src/main/java/fr/hoenheimsports/backend/contactservice/services/ContactService.java
+++ backend/src/main/java/fr/hoenheimsports/backend/contactservice/services/ContactService.java
@@ -2,6 +2,7 @@ package fr.hoenheimsports.backend.contactservice.services;
 
 import fr.hoenheimsports.backend.contactservice.exceptions.MailServiceException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,8 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ContactService {
     private final JavaMailSender javaMailSender;
-    private final static String ADMINISTRATOR_EMAIL_ADDRESS ="sebastien.burckhardt@hoenheimsports.fr";
+    @Value("${contact.administrator.email.address}")
+    private String ADMINISTRATOR_EMAIL_ADDRESS;
 
 
     /**
